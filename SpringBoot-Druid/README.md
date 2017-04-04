526289c988eb7e5ed2b33f50d153e7dcc265a9fd
lengchuan <lishuijun1992@gmail.com>
2017-04-04 19:21:50 +0800

    基础框架

---
+++ SpringBoot-Druid/README.md
@@ -0,0 +1,12 @@
+# SpringBoot使用阿里数据库连接池Druid以及多数据源配置
+
+# 引入依赖
+```aidl
+<!-- https://mvnrepository.com/artifact/com.alibaba/druid -->
+<dependency>
+    <groupId>com.alibaba</groupId>
+    <artifactId>druid</artifactId>
+    <version>1.0.29</version>
+</dependency>
+
+```
---
+++ SpringBoot-Druid/src/main/java/com/App.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lsj <lishuijun1992@gmail.com>
+ * @date 17-4-4
+ */
+@SpringBootApplication
+//@MapperScan(value = "com.lc.springBoot.mapper")//需要扫描的mapper接口所在包
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
